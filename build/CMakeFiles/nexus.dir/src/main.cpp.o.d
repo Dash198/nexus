ba5253017bb5c0850053ea73839c54b21e906e10
Dash198 <92976625+Dash198@users.noreply.github.com>
2026-01-28 12:08:36 +0530

    implemented readdir, open and read functionality, ls and cat now work

--- build/CMakeFiles/nexus.dir/src/main.cpp.o.d
+++ build/CMakeFiles/nexus.dir/src/main.cpp.o.d
@@ -1,39 +1,39 @@
 CMakeFiles/nexus.dir/src/main.cpp.o: \
  /home/devansh/repos/nexus/src/main.cpp /usr/include/stdc-predef.h \
- /usr/include/fuse3/fuse.h /usr/include/fuse3/fuse_common.h \
+ /usr/include/fcntl.h /usr/include/features.h \
+ /usr/include/features-time64.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/fcntl.h /usr/include/bits/fcntl-linux.h \
+ /usr/include/bits/types/struct_iovec.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stddef.h \
+ /usr/include/linux/falloc.h /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/stat.h \
+ /usr/include/bits/struct_stat.h /usr/include/fuse3/fuse.h \
+ /usr/include/fuse3/fuse_common.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h \
  /usr/include/fuse3/libfuse_config.h /usr/include/fuse3/fuse_opt.h \
  /usr/include/fuse3/fuse_log.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdint.h \
  /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
- /usr/include/features.h /usr/include/features-time64.h \
- /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
- /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
- /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
- /usr/include/bits/types.h /usr/include/bits/typesizes.h \
- /usr/include/bits/time64.h /usr/include/bits/wchar.h \
- /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
- /usr/include/bits/stdint-least.h /usr/include/sys/types.h \
- /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
- /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stddef.h \
- /usr/include/endian.h /usr/include/bits/endian.h \
- /usr/include/bits/endianness.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h /usr/include/bits/stdint-least.h \
+ /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/endian.h /usr/include/bits/byteswap.h \
  /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
  /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
  /usr/include/bits/types/__sigset_t.h \
  /usr/include/bits/types/struct_timeval.h \
- /usr/include/bits/types/struct_timespec.h \
  /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
  /usr/include/bits/pthreadtypes-arch.h \
  /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
  /usr/include/bits/struct_rwlock.h /usr/include/assert.h \
- /usr/include/fcntl.h /usr/include/bits/fcntl.h \
- /usr/include/bits/fcntl-linux.h /usr/include/bits/types/struct_iovec.h \
- /usr/include/linux/falloc.h /usr/include/bits/stat.h \
- /usr/include/bits/struct_stat.h /usr/include/time.h \
- /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
  /usr/include/bits/types/struct_tm.h \
  /usr/include/bits/types/struct_itimerspec.h \
  /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
@@ -49,8 +49,17 @@ CMakeFiles/nexus.dir/src/main.cpp.o: \
  /usr/include/bits/types/struct_statx.h /usr/include/sys/statvfs.h \
  /usr/include/bits/statvfs.h /usr/include/sys/uio.h \
  /usr/include/bits/uio_lim.h /usr/include/bits/uio-ext.h \
- /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
- /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/linux/sched/types.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h /usr/include/stdio.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
  /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
  /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
  /usr/include/bits/types/cookie_io_functions_t.h \
@@ -90,12 +99,6 @@ CMakeFiles/nexus.dir/src/main.cpp.o: \
  /usr/include/c++/15.2.1/ext/atomicity.h \
  /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/gthr.h \
  /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/gthr-default.h \
- /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
- /usr/include/linux/sched/types.h \
- /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
- /usr/include/bits/setjmp.h \
- /usr/include/bits/types/struct___jmp_buf_tag.h \
- /usr/include/bits/pthread_stack_min-dynamic.h \
  /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/atomic_word.h \
  /usr/include/sys/single_threaded.h \
  /usr/include/c++/15.2.1/bits/locale_classes.h \
@@ -160,4 +163,7 @@ CMakeFiles/nexus.dir/src/main.cpp.o: \
  /usr/include/c++/15.2.1/bits/locale_facets.tcc \
  /usr/include/c++/15.2.1/bits/basic_ios.tcc \
  /usr/include/c++/15.2.1/bits/ostream.tcc /usr/include/c++/15.2.1/istream \
- /usr/include/c++/15.2.1/bits/istream.tcc
+ /usr/include/c++/15.2.1/bits/istream.tcc /usr/include/dirent.h \
+ /usr/include/bits/dirent.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/dirent_ext.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 // Remove the #define FUSE_USE_VERSION 31 here (we moved it to CMake)
 
 // Use the explicit path to avoid confusion with v2
+#include <fcntl.h>
 #include <fuse3/fuse.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <iostream>
+#include <dirent.h>
 
 // Run once, when the fileesystem is mounted.
 static void* nexus_init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
@@ -57,9 +61,138 @@ int nexus_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *f
      }
 }
 
+int nexus_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags){
+    /*
+     * Read the contents of a directory
+     *
+     * Args:
+     *  path: Path to the directory we want to read (relative to filesystem)
+     *  buf: Buffer to write to, written by filler
+     *  filler: The helper function which conveniently fills up the buffer for us
+     *  offset: Offset of the next directory entry, needed for multiple calls to readdir in complex filesystems
+     *  fi: Pointer with low-level details about this specific "open" action, ignore it for now.
+     *  flags: Any extra instructions from the kernel, ignore for now.
+     */
+
+    // Setup the source path
+    std::string source = "/home/devansh/repos/nexus/nexus_data";
+    std::string final_path = source+path;
+
+    // Open the directory
+    DIR *pDir = opendir(final_path.c_str());
+
+    // Error handling
+    if(pDir == nullptr){
+        // Return negative errno to the kernel, interpreted automatically
+        return -errno;
+    }
+
+    // Read loop
+    struct dirent *entry;
+    // Keep readung until NULL (end of directory)
+    while((entry = readdir(pDir))!=NULL){
+
+        // We usually want . and .. for proper navigation
+        // If they are filtered out, then 'cd ..' may not work as expected
+        // So let us pass everything through for now
+
+        // Call the filler
+        // buf: buffer to fill
+        // entry->d_name: simple filename (eg: 'image.png')
+        // NULL: We let the kernel see stats for later
+        // 0: Offset (default)
+
+        filler(buf, entry->d_name, NULL, 0, (fuse_fill_dir_flags)0);
+    }
+
+    // Cleanup and return success
+    closedir(pDir);
+    return 0;
+}
+
+int nexus_open(const char *path, struct fuse_file_info *fi){
+    /*
+     * Attaches a file descriptor to the specified file.
+     *
+     * Args:
+     *  path: The path to the directory
+     *  fi: File info struct, used here :)
+     */
+
+    // Set up the source
+    std::string source = "/home/devansh/repos/nexus/nexus_data";
+    std::string final_path = source + path;
+
+    // Attempt to open and attach a file descriptor to the directory in read-only mode (writing to be handled later)
+    int fd = open(final_path.c_str(), O_RDONLY);
+
+    // Error handling
+    if(fd==-1){
+        // Return the error to the kernel
+        return -errno;
+    }
+
+    // Pass the file descriptor to fi, to use in the read() method, and return.
+    fi->fh = fd;
+    return 0;
+}
+
+int nexus_read(const char *path, char *buff, size_t size, off_t offset, struct fuse_file_info *fi){
+    /*
+     * Attempt to read a given file
+     *
+     * Args:
+     *  path: Path to the file
+     *  buff: Buffer to write to
+     *  size: Number of bytes to read
+     *  offset: Offset to start reading from
+     *  fi: File info struct, contains attached file descriptor
+     */
+
+    // Extract the file descriptor
+    int fd = fi->fh;
+
+    // Handle invalid descriptor
+    if(fd==-1){
+        return -errno;
+    }
+
+    // Read bytes from the file
+    int res = pread(fd, buff, size, offset);
+
+    // Error handling
+    if(res==-1){
+        res = -errno;
+    }
+
+    // Return res
+    return res;
+}
+
+int nexus_release(const char *path, struct fuse_file_info *fi){
+    /*
+     * Called when user is completely done with the file
+     *
+     * Args:
+     *  path: Path to the file
+     *  fi: Info about the file
+     */
+
+     // Acknowledge the path but do not use it
+    (void) path;
+    // Close the file descriptor we opened
+    close(fi->fh);
+
+    return 0;
+}
+
 // The "Employee Handbook" - Defines what function to run for each request and what all we are capable of.
 static struct fuse_operations nexus_oper = {
     .getattr = nexus_getattr,
+    .open = nexus_open,
+    .read = nexus_read,
+    .release = nexus_release,
+    .readdir = nexus_readdir,
     .init = nexus_init,
 };
 
